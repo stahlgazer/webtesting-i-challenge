cbe1b15e92eec2296c0bb6256616a896820c13a2
gavin stahl <stahlgavin@gmail.com>
2020-04-23 04:28:03 -0700

    completed some tests, might add more

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,59 @@ module.exports = {
   get,
 };
 
+// const item = {
+//   name: "item name",
+//   durability: 0-100,
+//   enhancement: 0-20
+// }
+
+// When enhancement succeeds
+// The item's enhancement increases by 1.
+// If the item enhancement level is 20, the enhancement level is not changed.
+// The durability of the item is not changed.
+
+// success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
 
+// When enhancement fails
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
+// fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
 function fail(item) {
+  if (item.enhancement < 15) {
+    if (item.durability >= 5) {
+      item.durability -= 5;
+    } else {
+      item.durability -= item.durability;
+    }
+  }
+  if (item.enhancement >= 15) {
+    if (item.durability >= 10) {
+      item.durability -= 10;
+    } else {
+      item.durability -= item.durability;
+    }
+  }
+  if (item.enhancement > 16) {
+    item.enhancement -= 1;
+  }
   return { ...item };
 }
 
+// repair(item) method that accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
 function repair(item) {
+  item.durability = 100;
   return { ...item };
 }
 
+// stretch
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,46 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const item = {
+  name: "night sword",
+  durability: 25,
+  enhancement: 10,
+};
+
+describe("enhancer.js", function () {
+  describe("repair", function () {
+    it("should set item durability to 100", function () {
+      enhancer.repair(item);
+      console.log(item);
+      expect(item.durability).toBe(100);
+    });
+  });
+});
+
+describe("succeed", function () {
+  it("should increase item enhancement by 1", function () {
+    enhancer.succeed(item);
+    console.log(item);
+    if (item.enhancement < 20) {
+      expect(item.enhancement).toBe(11);
+    } else {
+      expect(item.enhancement).toBe(10);
+    }
+  });
+});
+
+describe("fail", function () {
+  it("should either decrease durability by 5 or 10, or decrease enhancement level by 1", function () {
+    enhancer.fail(item);
+    console.log(item);
+    if (item.enhancement < 15) {
+      expect(item.durability).toBe(95);
+    }
+    if (item.enhancement >= 15) {
+      expect(item.durability).toBe(90);
+    }
+    if (item.enhancement > 16) {
+      expect(item.enhancement).toBe(9);
+    }
+  });
+});
